282b72e4502f6fb3166d37ff4c1a610dc12f0123
Romario Alves <romariogn10@gmail.com>
2022-10-05 17:48:47 -0300

    Feat(Listagem resposiva): add

--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -1,13 +1,19 @@
 import { Flex, Avatar, Box, Text } from "@chakra-ui/react";
 
-export function Profile() {
+interface ProfileProps {
+  showProfileData?: boolean
+}
+
+export function Profile({ showProfileData = true }: ProfileProps) {
   return (
     <Flex align="center">
-      <Box mr="4" textAlign="right">
+      { showProfileData && (
+        <Box mr="4" textAlign="right">
         <Text>Romário Alves</Text>
         <Text color="gray.300" fontSize="small">romariogn10@gmail.com</Text>
       </Box>
-
+      ) }
+      
       <Avatar size="md" name="Romário Alves" src="https://avatars.githubusercontent.com/u/59178255?v=4" />
     </Flex>
 
